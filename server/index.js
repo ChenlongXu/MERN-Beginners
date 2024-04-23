693aadd4fa4e56b4dec06136c6bc54c9f0507541
ChenlongXu <128095894+ChenlongXu@users.noreply.github.com>
2024-04-22 23:37:28 -0700

    Update the get() method

--- server/index.js
+++ server/index.js
@@ -2,24 +2,34 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const UserModel = require("./models/Users");
-
 const cors = require("cors");
 
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(
-  "mongodb+srv://user123:Password123Tech@cluster0.j7fql.mongodb.net/merntutorial?retryWrites=true&w=majority"
-);
+mongoose
+  .connect(
+    "mongodb+srv://mltriasl:Hazitagen2110@cluster0.mjdp36a.mongodb.net/MERN?retryWrites=true&w=majority&appName=Cluster0",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 app.get("/getUsers", (req, res) => {
-  UserModel.find({}, (err, result) => {
-    if (err) {
-      res.json(err);
-    } else {
+  console.log("Route /getUsers was called");
+  UserModel.find({})
+    .then((result) => {
+      if (result.length === 0) {
+        console.log("No users found");
+        return res.status(404).send("No users found.");
+      }
+      console.log("Users found:", result);
       res.json(result);
-    }
-  });
+    })
+    .catch((err) => {
+      console.error("Error retrieving users:", err);
+      res.status(500).json({ error: "Internal server error" });
+    });
 });
 
 app.post("/createUser", async (req, res) => {
@@ -31,5 +41,5 @@ app.post("/createUser", async (req, res) => {
 });
 
 app.listen(3001, () => {
-  console.log("SERVER RUNS PERFECTLY!");
+  console.log("server runs");
 });
